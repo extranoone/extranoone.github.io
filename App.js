3816bfa46e115c833c7a6034a49bc12ab16d667c
extranoone <72867944+extranoone@users.noreply.github.com>
2020-10-14 18:09:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './App.css';
+// import DemoCarousel from './Image'
+
+function App() {
+  return (
+    <div >
+      <p class="p2"><center>THE ORGANISATION</center></p>
+      <p class="p1"><strong><i>Khushaamdeed!</i></strong> We are a harmonious association of enthusiastic poetry lovers from different regions of India who will serve you with valuable and authentic learning content, strategies and equipment to help you enhance your knowledge of Urdu language and poetry as well. This will be done under concrete guidance of experts via offline and online mode. We will be hosting workshops, lectures, events and blogs. Pertaining to all approachable regions, we have a Publishing Department that intends to come up with a number of binders that you would cherish.</p>
+      <p class="p1"><center><b>Nahi’n khel ay <span>DaaG</span> yaaro’n se keh do<br />
+Ki aati hai urdu zabaa’n aate- aate</b></center></p>
+
+<div class="row">
+  <div class="column">
+  <iframe width="100%" height="410" src="https://www.youtube.com/embed/XHOmBV4js_E" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+  <div class="column1" width="100%">
+  It is no hidden truth that the foundation of any culture, any civilization lies in its Language: verbal and non-verbal. Our ways of life, social demeanor, cuisines and fashion all have an interplay of manifestations and modulations of the language we speak and write.What is of immense significance here is the fact that preserving or sustaining any culture or civilization rests with the conservation of its language. Additionally, this fact can not be ignored that this language primarily is the written one i.e. Literature and not the spoken one which is the language of the commoners.
+  </div>
+</div>
+{/* <DemoCarousel > </DemoCarousel> */}
+    </div>
+    
+     
+  );
+}
+
+export default App;
